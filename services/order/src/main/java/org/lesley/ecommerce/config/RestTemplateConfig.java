8960ead4961ca8c9cfe8a12df6cb5b304786284c
LesleyFopa <fopa.lesley@gmail.com>
2026-02-19 18:29:11 +0100

    resolution des conflits de securite avec web client

--- services/order/src/main/java/org/lesley/ecommerce/config/RestTemplateConfig.java
+++ services/order/src/main/java/org/lesley/ecommerce/config/RestTemplateConfig.java
@@ -2,14 +2,29 @@ package org.lesley.ecommerce.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import java.util.Collections;
 
 @Configuration
 public class RestTemplateConfig {
 
     @Bean
     public RestTemplate restTemplate() {
-
-        return new RestTemplate();
+        RestTemplate restTemplate = new RestTemplate();
+        restTemplate.setInterceptors(Collections.singletonList((request, body, execution) -> {
+            ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+            if (attributes != null) {
+                String authHeader = attributes.getRequest().getHeader("Authorization");
+                if (authHeader != null && !authHeader.isEmpty()) {
+                    request.getHeaders().add("Authorization", authHeader);
+                }
+            }
+            return execution.execute(request, body);
+        }));
+        return restTemplate;
     }
 }
